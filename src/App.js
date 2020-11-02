f56ddb7594629f9b065ce20ded7cd06f800dd8c1
Mak-Pro <maksprocode@gmail.com>
2020-11-02 09:43:10 +0200

    setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	state = {
+		
+	}
+
+	render() {
+
+		return (
+			<div className="content__container">
+				Content
+			</div>
+	  );
+	}
+  
+}
+
+export default App;
